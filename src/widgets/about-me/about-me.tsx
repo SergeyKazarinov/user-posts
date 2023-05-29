4aa7d56c2dabfa516c5351eaa0e75c56dffe278a
Sergey Kazarinov <kazarinov092@gmail.com>
2023-05-29 12:37:26 +0500

    fix(about-me): fix semantic tags

--- src/widgets/about-me/about-me.tsx
+++ src/widgets/about-me/about-me.tsx
@@ -12,14 +12,15 @@ const AboutMe: FC<AboutMeProps> = () => (
       <Card.Body>
         <Card.Title className='text-center text-primary display-2 lead'>Обо мне</Card.Title>
         <Card.Subtitle className='mt-5 display-5'>Привет. Меня зовут Сергей!</Card.Subtitle>
-        <Card.Text className='mt-5 display-6 text-start'>Здесь всё просто &#128522;
-          <ListGroup className='text-start mt-4 mb-4 display-6'>
-            <ListGroupItem className='bg-secondary'>Информация обо мне</ListGroupItem>
-            <ListGroupItem className='bg-secondary'>Информация о моих Hard и Soft-скилах </ListGroupItem>
-            <ListGroupItem className='bg-secondary'>Мои проекты</ListGroupItem>
-            <ListGroupItem className='bg-secondary'>Ссылка на GitHub</ListGroupItem>
-            <ListGroupItem className='bg-secondary'>Мои контакты</ListGroupItem>
-          </ListGroup>
+        <Card.Text className='mt-5 display-6 text-start'>Здесь всё просто &#128522;</Card.Text>
+        <ListGroup className='text-start mt-4 mb-4 display-6'>
+          <ListGroupItem className='bg-secondary'>Информация обо мне</ListGroupItem>
+          <ListGroupItem className='bg-secondary'>Информация о моих Hard и Soft-скилах </ListGroupItem>
+          <ListGroupItem className='bg-secondary'>Мои проекты</ListGroupItem>
+          <ListGroupItem className='bg-secondary'>Ссылка на GitHub</ListGroupItem>
+          <ListGroupItem className='bg-secondary'>Мои контакты</ListGroupItem>
+        </ListGroup>
+        <Card.Text className='mt-5 display-6 text-start'>
           {'Есть на моем '}
           <Card.Link href='https://kazarinov.nomoredomains.club/' className='text-info text-decoration-none' target='_blank'>сайте-портфолио.</Card.Link>
         </Card.Text>
